6a375e9e1fc3f69a2f235e21b9c8ed3f28c48862
Mitchell Bryson <mitchelljbryson@gmail.com>
2021-04-26 17:43:56 +0100

    dist

---
+++ js/chunk-2d0ac08b.3158b3ed.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ac08b"],{"188f":function(e,t,n){"use strict";n.r(t);var r=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"items"},e._l(e.items,(function(e){return n("Item",{key:e.id,attrs:{item:e}})})),1)},i=[],s=(n("d3b7"),{props:{items:{required:!0,type:Array}},components:{Item:function(){return n.e("chunk-156d5d2f").then(n.bind(null,"5e7d"))}}}),u=s,c=n("2877"),l=Object(c["a"])(u,r,i,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0ac08b.3158b3ed.js.map
